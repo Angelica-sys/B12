c20919f38aafc8dc112f05f4b3f11746c2c49a4d
Angelica-sys <62008916+Angelica-sys@users.noreply.github.com>
2020-12-16 14:25:35 +0100

    Uppdaterat runner med tomma getters.
    Skal för klassen färdig.

--- src/main/java/APIRunner.java
+++ src/main/java/APIRunner.java
@@ -1,2 +1,38 @@
+import java.util.List;
+
+import static spark.Spark.*;
+
 public class APIRunner {
+
+    public static void main(String[] args) throws Exception {
+        port(5000);
+
+
+
+        get("/", (req, res) -> {
+            return "Hello world";
+        });
+
+
+        get("/:id", (req, res) -> {
+            return "Hello world";
+        });
+
+        post("/", (req, res) -> {
+
+            return "Hello world";
+        });
+        put("/:id", (req, res) -> {
+
+            return "Hello world ";
+
+        });
+
+        delete("/:id", (req, res) -> {
+
+            return "Hello world";
+        });
+
+    }
 }
+
