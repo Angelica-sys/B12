4eb8be08c37316c28d064a921277552e18fe8860
Angelica-sys <62008916+Angelica-sys@users.noreply.github.com>
2020-12-18 09:58:03 +0100

    API runner update
    Lagt till funktion i våra get, put, delete och post.

--- src/main/java/APIRunner.java
+++ src/main/java/APIRunner.java
@@ -8,30 +8,38 @@ public class APIRunner {
         port(5000);
 
 
-
+    //Get list of users
         get("/", (req, res) -> {
-            return "Hello world";
+            return "Here is the list of users";
         });
-
+    //Get user by id
 
         get("/:id", (req, res) -> {
-            return "Hello world";
+            return "Here is your user";
         });
+    //Get nutrition value for specific item
+
+    // get ("/:id, (req, res) -> {
+        // return "Here is your requested food item"});
+
 
         post("/", (req, res) -> {
 
-            return "Hello world";
+            return "You have registered a user";
         });
         put("/:id", (req, res) -> {
 
-            return "Hello world ";
+            return "You have updated you users nutrition list";
 
         });
 
         delete("/:id", (req, res) -> {
 
-            return "Hello world";
+            return "You have deleted a user";
         });
+
+        //delete (/:id, (req, res) ->{
+             // return "You have deleted a food item in a users list"; });
 
     }
 }
